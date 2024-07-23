0cf7454e4688e225eb237842286de9d349e7fbc6
노의빈 <hosan_1@naver.com>
2024-07-23 13:01:28 +0900

    init commit

---
+++ backend/smru/src/main/java/com/ssafy/smru/SmruApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.smru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmruApplication.class, args);
+	}
+
+}
