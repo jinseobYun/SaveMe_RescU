2b10c4dfd058e054582c89738358f186c50b3099
이상휘 <sanghwi47@naver.com>
2024-07-26 09:11:11 +0900

    Feat: LoginPage 화면 구현 /S11P12B305-69

--- frontend/web/src/layouts/BasicLayout.jsx
+++ frontend/web/src/layouts/BasicLayout.jsx
@@ -4,12 +4,14 @@ import "./BasicLayout.css";
 
 export default function BasicLayout({ children }) {
   return (
-    <container>
-      <header>
-        <BasicMenus></BasicMenus>
-      </header>
-      <main>{children}</main>
-      <footer></footer>
-    </container>
+    <>
+      <container>
+        <header>
+          <BasicMenus></BasicMenus>
+        </header>
+        <main>{children}</main>
+        <footer></footer>
+      </container>
+    </>
   );
 }
--- frontend/web/src/pages/LoginPage.jsx
+++ frontend/web/src/pages/LoginPage.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import "./LoginPage.css"
+import BasicLayout from '../layouts/BasicLayout';
 
 export default function LoginPage() {
   return (
-    <div>LoginPage입니다.</div>
+    <BasicLayout>
+      <div className="logincontainer">
+        <div className='login-info'>
+          <div className='info-text'>로그인 페이지</div>
+        </div>
+        <div className='login-input'>
+          <div className='id'>
+            <div className='input-tag'>아이디 입력</div>
+            <input className='input'></input>
+          </div>
+          <div className='password'>
+            <div className='input-tag'>비밀번호 입력</div>
+            <input className='input'></input>
+          </div>
+        </div>
+        <button onClick={()=> console.log("로그인")} className='login-button'>
+          <div className='login-text'>로그인</div>
+        </button>
+      </div>
+    </BasicLayout>
+    
   );
 }
