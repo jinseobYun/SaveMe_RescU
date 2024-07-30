172afe4f58b57b6f3391c1f1c0dd1154407386b2
이상휘 <sanghwi47@naver.com>
2024-07-30 13:08:57 +0900

    Fix : Text component 오류 수정

--- frontend/web/src/components/elements/Text.jsx
+++ frontend/web/src/components/elements/Text.jsx
@@ -3,37 +3,46 @@ import styled, { css } from "styled-components";
 
 const Text = ({
   children = null,
-  bold = false,
-  color = "var(--black-color-300)",
-  size = "14px",
-  margin = false,
-  title = false,
-  lineHeight = "16px",
+  $bold = false,
+  $color = "var(--black-color-300)",
+  $size = "12px",
+  $margin = false,
+  $title = false,
+  $lineHeight = "16px",
   _onClick = () => {},
 }) => {
+  const styles = {
+    $color,
+    $bold,
+    $size,
+    $margin,
+    $title,
+    $lineHeight,
+  };
   return (
-    <P color={color} onClick={_onClick}>
+    <P {...styles} onClick={_onClick}>
       {children}
     </P>
   );
 };
 
 const P = styled.p`
-  color: ${(props) => props.color};
-  font-size: ${(props) => props.size};
-  font-weight: ${(props) => (props.bold ? "600" : "400")};
+  color: ${(props) => props.$color};
+  font-size: ${(props) => props.$size};
+  font-weight: ${(props) => (props.$bold ? "700" : "400")};
   ${(props) =>
-    props.margin
+    props.$margin
       ? css`
-          margin: ${props.margin};
+          margin: ${props.$margin};
         `
       : "margin:0px"};
   ${(props) =>
-    props.title
+    props.$title
       ? css`
           font-family: "GmarketSansBold";
         `
       : ""};
-  line-height: ${(props) => props.lineHeight};
+  line-height: ${(props) => props.$lineHeight};
 `;
+
 export default Text;
