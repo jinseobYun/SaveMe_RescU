cac41af64b7b0ca9346f84e2309a186203e84fd1
Jeon-YJ1004 <dudwn5374@naver.com>
2024-08-06 17:42:09 +0900

    Fix:web rtc 프론트 연결 Resolve # 251-253

--- frontend/mobile/src/pages/ReportCallPage.jsx
+++ frontend/mobile/src/pages/ReportCallPage.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState, forwardRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
+import { io } from "socket.io-client";
 
 import VideocamOffIcon from "@mui/icons-material/VideocamOff";
 import VideocamIcon from "@mui/icons-material/Videocam";
@@ -12,21 +13,41 @@ import CallEndIcon from "@mui/icons-material/CallEnd";
 import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
 import SendIcon from "@mui/icons-material/Send";
 
-import { Button, Input, Grid, Text } from "@components/elements";
-import {
-  socket,
-  initSocketConnection,
-  getMedia,
-  getCameras,
-  myPeerConnection,
-  localStream,
-  remoteVideoStream,
-  handleSendMessage,
-} from "@/util/socket";
+import { Button, Text, Grid } from "@components/elements";
+const socket = io(import.meta.env.VITE_SOCKET_SERVER_URL, {
+  transports: ["websocket"],
+  autoConnect: false,
+  pingTimeout: 300000,
+});
+
+const configuration = {
+  iceServers: [
+    {
+      urls: import.meta.env.VITE_ICE_SERVERS,
+    },
+  ],
+};
+let localStream;
+let roomName;
+let myPeerConnection;
+let myDataChannel;
+let localVideoRef;
+let remoteVideoRef;
+function makeConnection() {
+  console.log("makeConnection");
+  myPeerConnection = new RTCPeerConnection(configuration);
+  myPeerConnection.addEventListener("icecandidate", (data) =>
+    socket.emit("ice", data.candidate, roomName)
+  );
+  myPeerConnection.addEventListener("addstream", (data) => {
+    console.log("pc.addStream", data);
+    remoteVideoRef.current.srcObject = data.stream;
+  });
+}
 
 const ReportCallPage = () => {
   //SECTION - user settings
-  const userId = "userID";
+
   const [isChatting, setIsChatting] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const [showMenuAll, setShowMenuAll] = useState(false);
@@ -37,8 +58,8 @@ const ReportCallPage = () => {
   const [cameras, setCameras] = useState([]);
 
   const navigate = useNavigate();
-  const localVideoRef = useRef(null);
-  const remoteVideoRef = useRef(null);
+  localVideoRef = useRef(null);
+  remoteVideoRef = useRef(null);
 
   const handleMuteClick = () => {
     const enabled = !muted;
@@ -58,7 +79,7 @@ const ReportCallPage = () => {
     if (isCameraFront) index = 0;
     else index = 1;
     setIsCameraFront(!isCameraFront);
-    await getMedia(cameras[index].deviceId);
+    9;
     if (myPeerConnection) {
       const videoTrack = localStream.getVideoTracks()[0];
       const videoSender = myPeerConnection
@@ -104,39 +125,32 @@ const ReportCallPage = () => {
     setIsChatting(true);
     setShowMenu(false);
     setShowMenuAll(false);
+    setChatBtnColor("var(--white-color-200)");
     localVideoRef.current.style.bottom = `70px`;
   };
 
   //SECTION - chatting
   const [chatBtnColor, setChatBtnColor] = useState();
-  const [chat, setChat] = useState([]);
-  const [messageInput, setMessageInput] = useState("");
-  const changeChatBtnColor = () => {
-    if (isChatArrive) {
-      setChatBtnColor("var(--main-yellow-color)");
-    } else {
-      setChatBtnColor("var(--white-color-200)");
-    }
+  const [chatlog, setChatlog] = useState([]);
+  const inputRef = useRef(null);
+  const [input, setInput] = useState("");
+
+  const onChangeMessage = (e) => {
+    setInput(e.target.value);
   };
-  useEffect(() => {
-    //TODO - 새로운 메시지를 수신할 때마다 chat 상태를 업데이트
-    // receiveMessage((data) => {
-    //   setChat((prevChat) => [
-    //     ...prevChat,
-    //     { alignment: "left", message: data.message },
-    //   ]);
-    //   console.log(chatWrapperRef.current.offsetHeight);
-    //   setChatWrapperHeight(chatWrapperRef.current.offsetHeight); // 채팅 높이 업데이트
-    // });
-  }, []);
+
   const handleMessageSubmit = (event) => {
     event.preventDefault();
-    if (messageInput.trim() !== "") {
-      // 메시지 전송
-      handleSendMessage(messageInput);
-      setChat([...chat, { alignment: "right", message: messageInput }]);
-      setMessageInput("");
-      setChatWrapperHeight(chatWrapperRef.current.offsetHeight); // 채팅 높이 업데이트
+    if (input.trim() !== "") {
+      try {
+        console.log(input);
+        myDataChannel.send(input);
+        setChatlog([...chatlog, { alignment: "right", message: input }]);
+        setChatWrapperHeight(chatWrapperRef.current.offsetHeight); // 채팅 높이 업데이트
+      } catch (e) {
+        console.log(e);
+      }
+      inputRef.current.value = "";
     }
   };
   const [chatWrapperHeight, setChatWrapperHeight] = useState(0); // 채팅 높이 상태 추가
@@ -149,16 +163,151 @@ const ReportCallPage = () => {
       localVideoRef.current.style.bottom = `${chatHeight}px`;
     }
   }, [chatWrapperHeight]);
+
   //SECTION - 신고방 접속
 
+  async function getCameras() {
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const cameras = devices.filter((device) => device.kind === "videoinput");
+
+      setCameras(cameras);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async function getMedia(deviceId) {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices
+      .filter((device) => device.kind === "videoinput")
+      .map((device) => device.deviceId);
+    const mikes = devices
+      .filter((device) => device.kind === "audioinput")
+      .map((device) => device.deviceId);
+    const initialConstrains = {
+      audio: true,
+      video: { facingMode: "user" },
+    };
+    const userConstrains = {
+      audio: mikes.includes(deviceId)
+        ? { deviceId: { exact: deviceId } }
+        : true,
+      video: cameras.includes(deviceId)
+        ? { deviceId: { exact: deviceId } }
+        : { facingMode: "user" },
+    };
+    try {
+      localStream = await navigator.mediaDevices.getUserMedia(
+        deviceId ? userConstrains : initialConstrains
+      );
+
+      if (localVideoRef.current) {
+        localVideoRef.current.srcObject = localStream;
+      }
+
+      // localStream.getAudioTracks()[0].enabled = !muted;
+      // localStream.getVideoTracks()[0].enabled = !cameraOff;
+      await myPeerConnection.addStream(localStream);
+      if (!deviceId) {
+        await getCameras();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  const [loading, setLoading] = useState(true);
+  async function initCall() {
+    makeConnection();
+    await getMedia();
+  }
   useEffect(() => {
-    initSocketConnection();
+    socket.connect();
+    socket.on("already_in_room", () =>
+      alert(`You are already in the other room.\nYou can enter only one room.`)
+    );
+
+    socket.on("is_full", (roomName) => {
+      //TODO - 방 번호 재요청 api
+      alert(`${roomName} is full.`);
+    });
+
+    socket.on("is_available", async (roomName) => {
+      console.log(`${roomName} is available`);
+      await initCall();
+      socket.emit("join_room", roomName);
+    });
+
+    socket.on("start_chat", () => {
+      myDataChannel = myPeerConnection.createDataChannel("chat");
+      myDataChannel.addEventListener("message", (event) => {
+        console.log(event.data);
+        if (isChatting) {
+          setChatWrapperHeight.current &&
+            setChatWrapperHeight(chatWrapperRef.current.offsetHeight);
+          console.log(chatWrapperRef.current.offsetHeight);
+        } else {
+          setChatBtnColor("var(--main-yellow-color)");
+        }
+      });
+
+      socket.emit("join_chat", roomName);
+    });
+
+    socket.on("join_chat", () => {
+      myPeerConnection.addEventListener("datachannel", (event) => {
+        myDataChannel = event.channel;
+        myDataChannel.addEventListener("message", (event) => {
+          setChatlog([...chatlog, { message: event.data, alignment: "left" }]);
+          if (isChatting) {
+            setChatWrapperHeight.current &&
+              setChatWrapperHeight(chatWrapperRef.current.offsetHeight);
+            console.log(chatWrapperRef.current.offsetHeight);
+          } else {
+            setChatBtnColor("var(--main-yellow-color)");
+          }
+        });
+      });
+    });
+
+    socket.on("send_offer", async () => {
+      const offer = await myPeerConnection.createOffer({
+        offerToReceiveAudio: true,
+        offerToReceiveVideo: true,
+      });
+      console.log("sendging offer", offer);
+      myPeerConnection.setLocalDescription(offer);
+      console.log(roomName, offer);
+      socket.emit("offer", offer, roomName);
+    });
+    socket.on("message", async (message) => {
+      console.log("message", message);
+    });
+
+    socket.on("offer", async (offer) => {
+      console.log("offer받음", offer);
+      myPeerConnection.setRemoteDescription(offer);
+      const answer = await myPeerConnection.createAnswer();
+      myPeerConnection.setLocalDescription(answer);
+      socket.emit("answer", answer, roomName);
+    });
+
+    socket.on("answer", (answer) =>
+      myPeerConnection.setRemoteDescription(answer)
+    );
+
+    socket.on("ice", (ice) => {
+      myPeerConnection.addIceCandidate(ice);
+      console.log("ice 받음", ice);
+    });
+
     //SECTION - 신고 요청 api
     //만약 로그인 되잇다면 로그인id도 같이
     //res로 받은 roodId넣어서 방에 들어갈 수 있는지 요청
     // socket.emit('check_room',roomId);
-    socket.emit("check_room", "1");
+    roomName = "1";
+    socket.emit("check_room", roomName);
     //TODO - 신고 전달 내용 채우기
+    const userId = "userID";
     const data = {
       userId: userId,
       roomId: "1",
@@ -167,16 +316,7 @@ const ReportCallPage = () => {
       reportReason: "신고 이유",
       reportTarget: "신고 대상",
     };
-    socket.emit("message", { type: "send_report_info", data }, "1");
-    localVideoRef.current.srcObject = localStream;
-    remoteVideoRef.current.srcObject = remoteVideoStream;
-
-    getCameras().then(setCameras);
-    getMedia().then((stream) => {
-      if (stream && localVideoRef.current) {
-        localVideoRef.current.srcObject = stream;
-      }
-    });
+    // socket.emit("message", { type: "send_report_info", data }, "1");
 
     // window.addEventListener("chatMessage", (event) => {
     //   setChatMessages((prevMessages) => [
@@ -191,14 +331,12 @@ const ReportCallPage = () => {
       }
     };
   }, []);
-  useEffect(() => {
-    console.log(myPeerConnection);
-    if (myPeerConnection) {
-      remoteVideoRef.current.srcObject = remoteVideoStream;
-    }
-  }, [remoteVideoStream]);
+
   const onClickCallEnd = () => {
-    hangup();
+    if (myPeerConnection) {
+      myPeerConnection.close();
+      myPeerConnection = null;
+    }
     remoteVideoRef.current.srcObject.getVideoTracks().forEach((track) => {
       track.stop();
       remoteVideoRef.current.srcObject.removeTrack(track);
@@ -208,13 +346,14 @@ const ReportCallPage = () => {
     navigate("/");
   };
   return (
-    <PeerVideo
-      onClick={onClickScreen}
-      ref={remoteVideoRef}
-      muted
-      autoPlay
-      playsInline
-    >
+    <>
+      <PeerVideo
+        onClick={onClickScreen}
+        ref={remoteVideoRef}
+        muted
+        autoPlay
+        playsInline
+      />
       {showMenuAll && (
         <VideoBtn>
           <Button
@@ -267,33 +406,39 @@ const ReportCallPage = () => {
 
       {isChatting ? (
         <ChattingWrapper ref={chatWrapperRef}>
-          {chat && (
+          {chatlog && (
             <ChattingContents>
-              {chat.map((message, index) => (
-                <ChattingMessage
+              {chatlog.map((message, index) => (
+                <Grid
                   key={index}
-                  alignment={message.alignment}
-                  message={message.message}
+                  $display="flex"
+                  $justify_content={
+                    message.alignment === "right" ? "flex-end" : "flex-start"
+                  }
+                  $align_items=""
                 >
-                  <Text children={message.message} $size="2rem" />
-                </ChattingMessage>
+                  <ChattingMessage
+                    alignment={message.alignment}
+                    message={message.message}
+                  >
+                    <Text children={message.message} $size="2rem" />
+                  </ChattingMessage>
+                </Grid>
               ))}
             </ChattingContents>
           )}
           <ChatInputBox>
-            <input
-              type="text"
-              value={messageInput}
-              onChange={(e) => setMessageInput(e.target.value)}
-              onKeyPress={(e) => e.key === "Enter" && handleMessageSubmit(e)}
-            />
-            <Button
+            <input type="text" ref={inputRef} onChange={onChangeMessage} />
+            <button onClick={handleMessageSubmit}>
+              <SendIcon fontSize="large" />
+            </button>
+            {/* <Button
               _onClick={handleMessageSubmit}
               $width="39px"
               $height="48px"
               $bg={{ default: "transparent" }}
-              children={<SendIcon fontSize="large" />}
-            />
+              children={}
+            /> */}
           </ChatInputBox>
         </ChattingWrapper>
       ) : (
@@ -310,17 +455,19 @@ const ReportCallPage = () => {
       )}
 
       <MyVideo ref={localVideoRef} autoPlay />
-    </PeerVideo>
+    </>
   );
 };
-export default ReportCallPage;
 
-const PeerVideo = styled.div`
+const PeerVideo = styled.video`
   width: 100%;
   height: 100%;
   flex-shrink: 0;
   position: relative;
   background-color: pink;
+  transform: rotateY(180deg);
+  -webkit-transform: rotateY(180deg); /* Safari and Chrome */
+  -moz-transform: rotateY(180deg); /* Firefox */
 `;
 const MyVideo = styled.video`
   left: 1rem;
@@ -332,6 +479,9 @@ const MyVideo = styled.video`
   position: absolute;
   background-color: black;
   z-index: 1;
+  transform: rotateY(180deg);
+  -webkit-transform: rotateY(180deg); /* Safari and Chrome */
+  -moz-transform: rotateY(180deg); /* Firefox */
 `;
 const VideoBtn = styled.div`
   height: 55px;
@@ -397,12 +547,12 @@ const ChattingContents = styled.div`
   }
 `;
 
-// justify-content: ${({ alignment }) =>
-//   alignment === "right" ? "flex-end" : "flex-start"};
 const ChattingMessage = styled.div`
   display: flex;
   margin: 5px 0;
   padding: 10px;
+  align-items: ${({ alignment }) =>
+    alignment === "right" ? "flex-end" : "flex-start"};
   background-color: ${({ alignment }) =>
     alignment === "right"
       ? "var(--main-yellow-color)"
@@ -425,3 +575,5 @@ const ChatInputBox = styled.div`
     margin-right: 10px;
   }
 `;
+
+export default ReportCallPage;
