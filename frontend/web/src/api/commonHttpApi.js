a80b57e4eeaa6b012fd20743b5b76e7ceb810375
이상휘 <sanghwi47@naver.com>
2024-08-09 10:17:52 +0900

    Chore: 테스트용 호출 주소 수정

--- frontend/web/src/api/commonHttpApi.js
+++ frontend/web/src/api/commonHttpApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const http = axios.create({
-  // baseURL: 'https://i11b305.p.ssafy.io/openvidu',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://i11b305.p.ssafy.io/openvidu',
+  // baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Basic ' + btoa('OPENVIDUAPP:YSnUClWgXb8D4NQjSQGvnTX07bOEpy')
