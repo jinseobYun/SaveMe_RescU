ba5f29574700b52b8f03211fcb3a50957faf54b9
이상휘 <sanghwi47@naver.com>
2024-08-10 20:01:19 +0900

    Test: Openvidu 테스트 api주소변경

--- frontend/web/src/api/commonHttpApi.js
+++ frontend/web/src/api/commonHttpApi.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const http = axios.create({
-  // baseURL: 'https://i11b305.p.ssafy.io/openvidu',
+  baseURL: 'https://i11b305.p.ssafy.io/webrtc',
   // baseURL: 'http://localhost:5000',
-  baseURL: 'http://localhost:5442',
   headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Basic ' + btoa('OPENVIDUAPP:YSnUClWgXb8D4NQjSQGvnTX07bOEpy')
