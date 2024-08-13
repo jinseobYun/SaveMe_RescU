55e9bc8ae421caa1a4d0cbd0cabd175078feede1
이상휘 <sanghwi47@naver.com>
2024-08-14 01:04:33 +0900

    Fix: MapPage 초기값 오류 수정 /S11P12B305-299

--- frontend/web/src/pages/MapPage.jsx
+++ frontend/web/src/pages/MapPage.jsx
@@ -3,28 +3,31 @@ import KakaoMap from "../components/map/KakaoMap";
 import EmergencyList from "../components/map/EmergencyList";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEmergencyList } from "../slices/emergencySlice";
+import { useNavigate } from "react-router-dom";
 
 import "./MapPage.css";
 
 export default function Map() {
   const dispatch = useDispatch();
+  const nav = useNavigate();
   const { items, selectedHospital, route } = useSelector(
     (state) => state.emergencySlice
   );
 
   const [showAllMarkers, setShowAllMarkers] = useState(true);
-  const [latlon, setLatlon] = useState({
-    lat: 36.3553193257957, // 기본 값
-    lon: 127.29820111515, // 기본 값
-  });
+  const [latlon, setLatlon] = useState(null);
 
   const handleShowAllMarkers = () => {
     setShowAllMarkers(!showAllMarkers);
   };
 
-  // localStorage에서 값을 불러와 latlon에 할당
+  const handleGoMain = () => {
+    nav("/main");
+  };
+
+  // 로컬 스토리지나 세션 스토리지에서 값을 불러와 latlon에 할당
   useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("reportData"));
+    const storedData = JSON.parse(localStorage.getItem("reportData")) || JSON.parse(sessionStorage.getItem("reportData"));
     if (storedData) {
       setLatlon({
         lat: storedData.latitude,
@@ -33,7 +36,7 @@ export default function Map() {
     }
   }, []);
 
-  // localStorage가 변경되면 latlon을 업데이트
+  // 로컬 스토리지나 세션 스토리지가 변경되면 latlon을 업데이트
   useEffect(() => {
     const handleStorageChange = (event) => {
       if (event.key === "reportData") {
@@ -55,10 +58,12 @@ export default function Map() {
     };
   }, []);
 
-  // latlon이 변경될 때마다 데이터를 가져옴
+  // latlon이 있을 때만 데이터를 가져옴
   useEffect(() => {
-    console.log(latlon.lat, latlon.lon);
-    dispatch(fetchEmergencyList(latlon));
+    if (latlon) {
+      console.log(latlon.lat, latlon.lon);
+      dispatch(fetchEmergencyList(latlon));
+    }
   }, [dispatch, latlon]);
 
   // 페이지 전체 스크롤 제거
@@ -90,6 +95,9 @@ export default function Map() {
         <div className="controls">
           <button className="mappage-btn" onClick={handleShowAllMarkers}>
             자세히보기
+          </button>
+          <button className="mappage-btn" onClick={handleGoMain}>
+            메인페이지로
           </button>
         </div>
         <EmergencyList items={items} />
